0b732e3dff21909982ad2b49838bbe9256a89f16
Jonathan Scheiber <jonathan@scheiber.dev>
2025-03-05 01:16:37 -0500

    update subtitles

--- src/utils/articles.ts
+++ src/utils/articles.ts
@@ -26,7 +26,7 @@ export const articles: Article[] = [
     id: "pain-is-real",
     title: "The Pain Is Real",
     subtitle:
-      'It\'s not "just in your mind". The pain you feel from being rejected is a traumatic event as strong as any physical pain and activates some of the same areas of the brain.',
+      'It\'s not "just in your mind". The pain you feel from being rejected is a traumatic event as strong as any physical pain and activates some of the same areas of the brain. Understand the physical effects of what happens to all of us when we are rejected.',
     slug: "pain-is-real",
     imageUrl: "/images/pain-is-real.jpg",
   },
@@ -41,7 +41,7 @@ export const articles: Article[] = [
     id: "no-contact",
     title: "No Contact",
     subtitle:
-      "Contact with your ex- is like cutting an open wound over and over again. Any contact at all, any reminders, makes healing harder.",
+      "Contact with your ex- is like cutting an open wound over and over again. Any contact at all, any reminders, makes healing harder. No contact is by far the most important step, and also the most difficult. Learn why to do it and how.",
     slug: "no-contact",
     imageUrl: "/images/no-contact.jpg",
   },
@@ -57,7 +57,7 @@ export const articles: Article[] = [
     id: "closure",
     title: "Closure",
     subtitle:
-      "Everyone wants closure after a breakup. Why did it happen? Why? Why, why, why? But closure is not important for your healing.",
+      "Everyone wants closure after a breakup. Why did it happen? Why? Why, why, why? But closure is not important for your healing. Understand why it doesn't matter... and how to do it right, if you still feel it's necessary.",
     slug: "closure",
     imageUrl: "/images/closure.jpg",
   },
@@ -65,7 +65,7 @@ export const articles: Article[] = [
     id: "remove-reminders",
     title: "Remove All Reminders",
     subtitle:
-      "It's obvious, but it's so hard to do: you have to get rid of anything and everything that reminds you of your ex-.",
+      "It's obvious, but it's so hard to do: you have to get rid of anything and everything that reminds you of your ex-. Anything which triggers memories of your ex- is harmful to you. Here's why… and several ideas of how to get rid of everything.",
     slug: "remove-reminders",
     imageUrl: "/images/remove-reminders.jpg",
   },
@@ -73,7 +73,7 @@ export const articles: Article[] = [
     id: "contact-from-ex",
     title: "Contact from the Ex",
     subtitle:
-      "You shouldn't contact your ex-. But what if your ex- contacts you? How do you interpret what the ex- says and does?",
+      "You shouldn't contact your ex-. But what if your ex- contacts you? How do you interpret what the ex- says and does? How do you respond?",
     slug: "contact-from-ex",
     imageUrl: "/images/contact-from-ex.jpg",
   },
@@ -81,7 +81,7 @@ export const articles: Article[] = [
     id: "bumping-into-ex",
     title: "Bumping into the Ex",
     subtitle:
-      "It's a difficult and stressful situation to bump into the ex- when you're still recovering from the breakup, so it's worth thinking about in advance.",
+      "It's a difficult and stressful situation to bump into the ex- when you're still recovering from the breakup, so it's worth thinking about in advance. Here are ideas and suggestions of how to handle it.",
     slug: "bumping-into-ex",
     imageUrl: "/images/bumping-into-ex.jpg",
   },
@@ -89,7 +89,7 @@ export const articles: Article[] = [
     id: "ex-special-occasions",
     title: "Ex's Special Occasions",
     subtitle:
-      "What do you do when your ex- has a big life event like a birthday, job promotion, or graduation? Acknowledge it? How? Ignore it?",
+      "What do you do when your ex- has a big life event like a birthday, job promotion, or graduation? Acknowledge it? How? Ignore it? It's a tricky situation which causes never-ending stress for many people following a breakup. Learn here how to handle it.",
     slug: "ex-special-occasions",
     imageUrl: "/images/ex-special-occasions.jpg",
   },
@@ -97,7 +97,7 @@ export const articles: Article[] = [
     id: "ex-with-new-partner",
     title: "Ex- with New Partner",
     subtitle:
-      "Your ex- will be with a new person someday. Maybe sooner, maybe later, but it will happen (or already has). And you're going to have to accept that.",
+      "Your ex- will be with a new person someday. Maybe sooner, maybe later, but it will happen (or already has). And you're going to have to accept that. Coping with this is hard while you're recovering. Here are suggestions of how to handle it.",
     slug: "ex-with-new-partner",
     imageUrl: "/images/ex-with-new-partner.jpg",
   },
@@ -105,7 +105,7 @@ export const articles: Article[] = [
     id: "getting-back-together",
     title: "Getting Back Together",
     subtitle:
-      "I know from deeply painful personal experience that getting back together with your ex- seems the only hope for life. The odds are very low.",
+      "I know from deeply painful personal experience that getting back together with your ex- seems the only hope for life. The odds are very low. Here's why… and what characterizes cases where the couple really does get back together. ",
     slug: "getting-back-together",
     imageUrl: "/images/getting-back-together.jpg",
   },
@@ -113,7 +113,7 @@ export const articles: Article[] = [
     id: "friends-family-support",
     title: "Friends, Family, Support",
     subtitle:
-      "It's obvious, but you shouldn't spend a lot of time alone. Get a team of people you can rely on: friends, family, posters on internet forums.",
+      "It's obvious, but you shouldn't spend a lot of time alone. Get a team of people you can rely on: friends, family, posters on internet forums. Here are strategies for how to use your support network effectively without damaging your relationship with them.",
     slug: "friends-family-support",
     imageUrl: "/images/friends-family-support.jpg",
   },
@@ -121,7 +121,7 @@ export const articles: Article[] = [
     id: "mental-work",
     title: "Mental Work",
     subtitle:
-      "You need to make a real mental effort to focus on YOU and YOUR future, not to obsess about your ex- and the past. It's hard. Very hard.",
+      "You need to make a real mental effort to focus on YOU and YOUR future, not to obsess about your ex- and the past. It's hard. Very hard. It's hard. Very hard. There's no easy switch to flip and instantly change your thinking. But there are things you can do. Learn how.",
     slug: "mental-work",
     imageUrl: "/images/mental-work.jpg",
   },
@@ -129,7 +129,7 @@ export const articles: Article[] = [
     id: "never-another-love",
     title: "Never Another Love",
     subtitle:
-      'The ex- is "the one" for you. You\'ll never find anyone else. You\'re doomed to die alone. Sigh... But on the other hand, everyone repeating to you "big ocean, lots of fish" doesn\'t help.',
+      'The ex- is "the one" for you. You\'ll never find anyone else. You\'re doomed to die alone. Sigh… But on the other hand, everyone repeating to you "big ocean, lots of fish" doesn\'t help. So here are realistic ways to look at this issue.',
     slug: "never-another-love",
     imageUrl: "/images/never-another-love.jpg",
   },
@@ -137,7 +137,7 @@ export const articles: Article[] = [
     id: "revenge",
     title: "Revenge",
     subtitle:
-      "The desire to hurt the person who hurt you is overwhelming. That's natural. And if there was betrayal as well... trust me, my mental revenge scenarios were detailed and gory.",
+      "The desire to hurt the person who hurt you is overwhelming. That's natural. And if there was betrayal as well… trust me, my mental revenge scenarios were detailed and gory. Here's what to do about it.",
     slug: "revenge",
     imageUrl: "/images/revenge.jpg",
   },
@@ -145,7 +145,7 @@ export const articles: Article[] = [
     id: "alcohol-drugs-medicine",
     title: "Alcohol, Drugs, Medicine",
     subtitle:
-      "It's natural that you want to dull the pain and try to escape. A magic pill to make it all go away would be great. But it's not going to help and usually just makes things worse.",
+      "It's natural that you want to dull the pain and try to escape. A magic pill to make it all go away would be great. But it's not going to help and usually just makes things worse. Understand what the problems are… and when it does make sense to use it.",
     slug: "alcohol-drugs-medicine",
     imageUrl: "/images/alcohol-drugs-medicine.jpg",
   },
@@ -153,7 +153,7 @@ export const articles: Article[] = [
     id: "health",
     title: "Health",
     subtitle:
-      "Your body goes through extreme physical duress following a divorce or breakup: sleep issues, stress hormones, food problems, lethargy, anxiety, and more.",
+      "Your body goes through extreme physical duress following a divorce or breakup: sleep issues, stress hormones, food problems, lethargy, anxiety, and more. Ignoring these issues will only worsen your suffering. Above anything else, you need to focus on your health. Here's how.",
     slug: "health",
     imageUrl: "/images/health.jpg",
   },
@@ -161,7 +161,7 @@ export const articles: Article[] = [
     id: "activities",
     title: "Activities",
     subtitle:
-      "After a break-up, everyone experiences a massive void. It's not just a hole in your heart… it's also literally a hole in your schedule: you have a huge amount of extra free time.",
+      "After a break-up, everyone experiences a massive void. It's not just a hole in your heart… it's also literally a hole in your schedule: you have a huge amount of extra free time. Wallowing in your misery only prolongs your recovery. Here are ideas for activities that will help.",
     slug: "activities",
     imageUrl: "/images/activities.jpg",
   },
@@ -169,7 +169,7 @@ export const articles: Article[] = [
     id: "rebound-relationships",
     title: "Rebound Relationships",
     subtitle:
-      "Casual sex, casual dating, rebound relationships: there's lots of debate about how helpful it can be to your recovery. It all depends on YOU and what's best for YOU.",
+      "Casual sex, casual dating, rebound relationships: there's lots of debate about how helpful it can be to your recovery. It all depends on YOU and what's best for YOU. Here are many pros and cons to help you think through the issue.",
     slug: "rebound-relationships",
     imageUrl: "/images/rebound-relationships.jpg",
   },
@@ -177,7 +177,7 @@ export const articles: Article[] = [
     id: "language",
     title: "Language",
     subtitle:
-      "As much as it hurts, you have to force yourself to make your language reflect the reality of the present... even when just talking to yourself.",
+      "As much as it hurts, you have to force yourself to make your language reflect the reality of the present… even when just talking to yourself. This person is no longer your significant other. They are your ex-. And your relationship no longer is, it was. Learn how your language reflects your thinking and learn how to change it.",
     slug: "language",
     imageUrl: "/images/language.jpg",
   },
@@ -185,7 +185,7 @@ export const articles: Article[] = [
     id: "costly-guides-plans",
     title: "Costly Guides & Plans",
     subtitle:
-      'A dumpee is in an incredibly vulnerable state and would pay anything for a "guaranteed solution" to end the pain and/or get the ex- back.',
+      'A dumpee is in an incredibly vulnerable state and would pay anything for a "guaranteed solution" to end the pain and/or get the ex- back. Thousands of internet guides take advantage and offer paid-for plans and strategies. Learn what they are and why you shouldn\'t waste your money.',
     slug: "costly-guides-plans",
     imageUrl: "/images/costly-guides-plans.jpg",
   },
@@ -193,7 +193,7 @@ export const articles: Article[] = [
     id: "youre-not-alone",
     title: "You're Not Alone",
     subtitle:
-      "After a breakup, it's easy to feel that no one is suffering as you are. But right now at this moment, there are many others who've come to this site for help.",
+      "After a breakup, it's easy to feel that no one is suffering as you are. But right now at this moment, there are many others who've come to this site for help. Real people with real breakup problems. Watch in real-time what queries brought them here.",
     slug: "youre-not-alone",
     imageUrl: "/images/youre-not-alone.jpg",
   },
